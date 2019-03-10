421f263892580519d4d44d40a2d7c1076c535821
Derek Haus <rumbleincybertron@gmail.com>
2019-03-10 19:31:55 +0900

    Splash Screen with Music Player

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2.5f;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Invoke("LoadFirstScene", levelLoadDelay);
+       audioSource = GetComponent<AudioSource>();   
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
