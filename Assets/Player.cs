d5bef33c663556f3a1a88b169334f15933d49f90
Derek Haus <rumbleincybertron@gmail.com>
2019-03-12 20:30:38 +0900

    Rotate Ship with Position and Throw

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,7 +8,14 @@ public class Player : MonoBehaviour
 {
     [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 4f;
-    [Tooltip("In m")] [SerializeField] float yRange = 2f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+    [SerializeField] float posPitchFactor = -5f;
+    [SerializeField] float ctrlPitchFactor = -25f;
+    [SerializeField] float posYawFactor = 5f;
+    [SerializeField] float ctrlRollFactor = -30f;
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +26,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPos = transform.localPosition.y * posPitchFactor;
+        float pitchDueToCtrlThrow = yThrow * ctrlPitchFactor;
+        float pitch = pitchDueToPos + pitchDueToCtrlThrow;
+
+        float yaw = transform.localPosition.x * posYawFactor;
+
+        float roll = xThrow * ctrlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
         float xOffset = xThrow * speed * Time.deltaTime;
         float yOffset = yThrow * speed * Time.deltaTime;
