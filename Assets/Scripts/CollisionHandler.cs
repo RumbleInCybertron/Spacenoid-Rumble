e9286ef82b47e65facc18380925e5f9ca2410af0
Derek Haus <rumbleincybertron@gmail.com>
2019-03-15 22:16:28 +0900

    Enabled deathFX GameObject
    used GameObject.SetActive() to enable the death particle explosion when the player triggers a collision with another game object.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;  // ok as long as this is the only script that loads scenes
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 2f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
+
     // TODO create code for triggering enemies, obstacles, terrain, etc
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        print("Player dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene()  // string referenced
+    {
+        SceneManager.LoadScene("Game");
     }
 }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -14,6 +14,6 @@ public class SceneLoader : MonoBehaviour
 
     void LoadFirstScene()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Game");
     }
 }
