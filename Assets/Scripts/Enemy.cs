9c33a374c36d0525605c9a85d9e4da696de80001
Derek Haus <rumbleincybertron@gmail.com>
2019-03-16 13:50:07 +0900

    Score Incrementation
    scorePerHit now placed on enemy. When a particle collision happens on an enemy it sends the scorePerHit to the ScoreBoard class.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,14 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 1234;
 
-    // Start is called before the first frame update
+    ScoreBoard scoreBoard;
+
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -20,14 +23,14 @@ public class Enemy : MonoBehaviour
         boxCollider.isTrigger = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    private void OnParticleCollision(GameObject other)
+    void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(this.gameObject);
