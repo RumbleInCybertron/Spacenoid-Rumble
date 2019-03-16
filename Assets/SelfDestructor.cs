3fb05673027060afe4785786caaea6f891754366
Derek Haus <rumbleincybertron@gmail.com>
2019-03-16 11:35:01 +0900

    Instantiating at Runtime
    Destroyed enemies moved to Spawned At Runtime parent. Also, the destroyed enemies' prefabs are removed after a customizable delay.

---
+++ Assets/SelfDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField] float selfDestructDelay = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, selfDestructDelay);
+    }
+
+    void Update()
+    {
+        
+    }
+}
